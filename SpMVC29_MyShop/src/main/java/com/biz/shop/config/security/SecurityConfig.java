a1166e7caa4a4726594a8acf6d5427d24026f735
freeswoo <freeswoo@naver.com>
2020-05-18 11:28:11 +0900

    2020-05-18

--- SpMVC29_MyShop/src/main/java/com/biz/shop/config/security/SecurityConfig.java
+++ SpMVC29_MyShop/src/main/java/com/biz/shop/config/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
@@ -66,7 +67,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return new StandardPBEStringEncryptor();
+		return new BCryptPasswordEncoder();
 				
 	}
 
